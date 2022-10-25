7c79f887d9dd4a24eda0aa0832dee2bcb3f0b749
zhchaoxing <zhchaoxing@hotmail.com>
2022-10-25 16:01:07 +0800

    Update prep_android_device.sh

--- prep_android_device.sh
+++ prep_android_device.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
  # Disable exit on non 0
-$ANDROID_HOME/platform-tools/adb kill-server
+"$ANDROID_HOME/platform-tools/adb" kill-server
 set +e
 
-kill $(ps aux | grep '[e]mulator' | awk '{print $2}')
+#kill "$(ps aux | grep '[e]mulator' | awk '{print $2}')"
+kill "$(pgrep '[e]mulator' | awk '{print $2}')"
 
 # Enable exit on non 0
 set -e
@@ -12,10 +13,10 @@ set -e
 sleep 20s # TODO: will optimize later
 
 # emulator -avd Android25 -wipe-data -no-snapshot &
-$ANDROID_HOME/emulator/emulator -avd Android33 -noaudio -no-window -accel on -ports 5556,5557 &
+"$ANDROID_HOME/emulator/emulator" -avd Android33 -noaudio -no-window -accel on -ports 5556,5557 &
 
 ANDROID_SERIAL=emulator-5556
 
 # wait for emulator to be up and fully booted, unlock screen
 
-$ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done; input keyevent 82'
+"$ANDROID_HOME/platform-tools/adb" wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done; input keyevent 82'
